2f9614c155225806844c74503a879a2fdeed80ba
Abhishekprajapati0 <140056290+Abhishekprajapati0@users.noreply.github.com>
2023-07-24 12:19:52 +0530

    Learn Javascript

---
+++ Create function.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>function</title>
+</head>
+<body>
+	<script>
+		function abhi(){
+			document.write("My selfIntro!");
+		}
+		function log(){
+			document.write("My name is Abhishek Prajapati.I belong to Bilthra Road Ballia (up). I am just 18 years old . I have compited 12th from S.I.C Inter college Bilthara road ballia.At the present time .I a doing Graduation B.A from Nagra Ballia (up). As will as I have completed (I.T Diploma ) My course name is (Full stack devlopment).  ");
+		}
+		abhi();
+		document.write("<br>")
+		log();
+	</script>
+
+</body>
+</html>
